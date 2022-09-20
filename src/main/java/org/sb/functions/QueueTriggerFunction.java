5f3b2c787f9acadc7186dd7f86c56120f05db4ad
sidgopal <12432468+sidgopal@users.noreply.github.com>
2022-09-20 16:07:48 +0530

    Update QueueTriggerFunction.java

--- src/main/java/org/sb/functions/QueueTriggerFunction.java
+++ src/main/java/org/sb/functions/QueueTriggerFunction.java
@@ -15,6 +15,6 @@ public class QueueTriggerFunction {
             @ServiceBusQueueTrigger(name = "message", queueName = "aksmsservicequeue", connection = "MyStorageConnectionAppSetting") String message,
             final ExecutionContext context
     ) {
-        context.getLogger().info("Hello Hi Java " + message);
+        context.getLogger().info("Service Bus Queue trigger function processed a message: " + message);
     }
 }
