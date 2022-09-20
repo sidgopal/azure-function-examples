3eb8c020a905db9a2aadabbe0defb160ad0c129f
macbookpro <siddharth_gopal@yahoo.com>
2022-09-20 15:53:51 +0530

    local func code

--- README.md
+++
@@ -1,2 +0,0 @@
-# azure-function-examples
-Azure function implementation to trigger based on message received from service bus queue
--- src/main/java/org/sb/functions/QueueTriggerFunction.java
+++ src/main/java/org/sb/functions/QueueTriggerFunction.java
@@ -1,4 +1,4 @@
-package org.sid.functions;
+package org.sb.functions;
 
 import com.microsoft.azure.functions.annotation.*;
 import com.microsoft.azure.functions.*;
@@ -10,13 +10,11 @@ public class QueueTriggerFunction {
     /**
      * This function will be invoked when a new message is received at the specified path. The message contents are provided as input to this function.
      */
-    @FunctionName("sbprocessor")
-    public void serviceBusProcess(
-            @ServiceBusQueueTrigger(name = "msg",
-                    queueName = "funcqueue",
-                    connection = "ServiceBusConnection") String message,
+    @FunctionName("QueueTrigger-Java")
+    public void run(
+            @ServiceBusQueueTrigger(name = "message", queueName = "aksmsservicequeue", connection = "MyStorageConnectionAppSetting") String message,
             final ExecutionContext context
     ) {
-        context.getLogger().info("Hello " + message);
+        context.getLogger().info("Java Queue trigger function processed a message: " + message);
     }
 }
